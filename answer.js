1a730a90bcc4bf76a33c4143e33e224ae7b41f6c
Tijuana22 <tijuanahunt39@gmail.com>
2022-12-12 10:42:07 -0500

    Wild Life answered

--- answer.js
+++ answer.js
@@ -38,11 +38,44 @@
 // 🔴 The commit message should read:
 // "Fizz Buzz answered"
 
+// for(let y = 0; y < 101; y++)
+// if(y % 3 == 0 && y % 5 == 0){
+//     console.log('FizzBuzz');
+// }else if ( y % 3 === 0){
+//     console.log('Fizz');
+// }else (y % 5 === 0)
+//     console.log('Buzz');
 
-for(let y = 0; y < 101; y++)
-if(y % 3 == 0 && y % 5 == 0){
-    console.log('FizzBuzz');
-}else if ( y % 3 === 0){
-    console.log('Fizz');
-}else (y % 5 === 0)
-    console.log('Buzz');
+
+
+// Wild Wild Life
+// Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// Give D'Art a second hometown by adding "Hawkins"
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+// 🔴 The commit message should read:
+// "Wild Wild Life answered"
+
+plantee.age = 5000;
+for(let a = 1; a <= 5001; a++){
+    plantee.age = a;
+}
+console.log(plantee);
+
+wolfy[3] = 'Gotham City';
+console.log(wolfy);
+
+dart.push('Hawkins');
+console.log(dart);
+
+wolfy.shift('Wolfy');
+wolfy.unshift('Gameboy');
+console.log(wolfy);
