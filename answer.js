7a675470381d6fbc194c5ab3de94193550cecc01
Tijuana22 <tijuanahunt39@gmail.com>
2022-12-12 09:40:18 -0500

    Easy Going answered

---
+++ answer.js
@@ -0,0 +1,29 @@
+// Part I: HTML & CSS
+// Prepare yourself for tomorrow's morning exercise! Watch the following videos on HTML and CSS. You will be working on practicing CSS in your Morning Exercise.
+
+// Rank you comfort with the following material on a scale of 0 - 5: 4
+
+
+// Part II: JavaScript Reps
+// Easy Going
+// Write a for loop that will log the numbers 1 through 20.
+// 🔴 The commit message should read:
+// "Easy Going answered"
+
+for(let x =0; x < 21; x++){
+    console.log(x);
+}
+
+
+
+// Get Even
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+// Hint: Think about the increment expression.
+
+// 🔴 The commit message should read:
+// "Get Even answered"
+
+
+
+// Fizz Buzz
