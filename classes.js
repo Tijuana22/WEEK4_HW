5d1fba28d40a5b817340d86f8578adb0fdddf4f7
Tijuana22 <tijuanahunt39@gmail.com>
2022-12-14 10:20:44 -0500

    created person class

--- classes.js
+++ classes.js
@@ -15,20 +15,103 @@
 // "created hamster class"
 // Easy Mode Make sure it works so far
 
-class Hamster {
+// class Hamster {
+//     constructor(name){
+//       this.owner = ''
+//       this.name = name
+//       this.price = 15
+//     }
+//     wheelRun(){
+//       console.log('squeak squeak')
+//     }
+//     eatFood(){
+//       console.log('nibble nibble')
+//     }
+//     getPrice(){
+//       return this.price
+//     }
+//   }
+
+//   let hamster = new Hamster('', '', 15);
+//   console.log(hamster, hamster.wheelRun(), hamster.eatFood(), hamster.getPrice());
+
+
+
+//   Person
+// attributes:
+
+// name - set name from parameter in constructor method
+// age - initially 0
+// height - initially 0
+// weight - initially 0
+// mood - integer starting at 0 initially
+// hamsters - empty array initially
+// bankAccount - initially set to 0
+// methods:
+
+// getName() - returns name
+// getAge() - returns age
+// getWeight() - returns weight
+// greet() - logs a message with person's name
+// eat() - increment weight, increment mood
+// exercise() - decrement weight
+// ageUp() - increment age, increment height, increment weight, decrement mood, increment bank account by 10 (birthday money)
+// buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice())
+// 🔴 Hard Mode Save & Commit your work!
+// Your commit message should read something like:
+// "created person class"
+// Easy Mode Keep Going and save and run code
+
+class Person {
     constructor(name){
-      this.owner = ''
-      this.name = name
-      this.price = 15
+        this.name = name
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
     }
-    wheelRun(){
-      console.log('squeak squeak')
+
+    getName(){
+        return this.name
     }
-    eatFood(){
-      console.log('nibble nibble')
+
+    getAge(){
+        return this.age
     }
-    getPrice(){
-      return this.price
+
+    getWeight(){
+      return this.weight
     }
-  }
-  
+
+    greet(){
+      console.log(`I am ${this.name} hello`)
+    }
+
+    eat(){
+      this.weight++
+      this.mood++
+    }
+
+    exercise(){
+      this.weight--
+    }
+
+    ageUp(){
+      this.age++
+      this.height++
+      this.weight++
+      this.mood--
+      this.bankAccount+=10
+    }
+
+    buyHamster(hamster){
+      this.hamsters.push(hamster)
+      this.mood+=10
+      this.bankAccount-=hamster.getPrice()
+    }
+    
+}
+let person = new Person('', 0, 0, 0, 0, [], 0);
+console.log(person, person.greet());
